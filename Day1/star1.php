0c9086e704441f0cbc78dd979aab87cbc4d96ae8
ArjG <agrewal12345@gmail.com>
2020-12-01 18:27:23 +0000

    both solutions

--- Day1/star1.php
+++ Day1/star1.php
@@ -2,29 +2,51 @@
 
 class Star
 {
-    function elves(){
 
-        $data = file("data.txt", FILE_IGNORE_NEW_LINES);
-        $i = 0;
-        $e = 0;
+    public $data;
+    public $i ;
+    public $e;
 
-        foreach($data as $d){
-            while($i < sizeof($data)){
-                while($e < sizeof($data)){
-                    if($d + $data[$i] + $data[$e] == 2020){
-                        return ($d * $data[$i] * $data[$e]);
-                    }
-                    $e++;
+    public function __construct()
+    {
+        $this->data = file("data.txt", FILE_IGNORE_NEW_LINES);
+        $this->i = 0;
+        $this->e = 0;
+    }
+
+    function elves1(){
+        
+        foreach($this->data as $d){
+            while($this->i < sizeof($this->data)){
+                if($d + $this->data[$this->i] == 2020){
+                    return $d * $this->data[$this->i];
                 }
-                $e = 0;
-                $i++;
+            $this->i++;
             }
-            $i = 0;
+            $this->i = 0;
+        }
+    }
+
+    function elves2(){
+        
+        foreach($this->data as $d){
+            while($this->i < sizeof($this->data)){
+                while($this->e < sizeof($this->data)){
+                    if($d + $this->data[$this->i] + $this->data[$this->e] == 2020){
+                        return ($d * $this->data[$this->i] * $this->data[$this->e]);
+                    }
+                    $this->e++;
+                }
+                $this->e = 0;
+                $this->i++;
+            }
+            $this->i = 0;
         }
     }
 }
 
     $me = new Star();
-    echo $me->elves();
+    echo ("STAR 1: " . $me->elves1());
+    echo ("<br><br>STAR 2: " . $me->elves2());
     
 ?>
